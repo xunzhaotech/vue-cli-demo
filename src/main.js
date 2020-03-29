b1b69d933a821d3b4197aa441e60f322f35e4a20
luyb <luyb@xunzhaotech.com>
2020-03-29 22:34:52 +0800

    feature -集成.vue文件

--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
 /*
  * @Author: your name
  * @Date: 2020-03-29 13:14:13
- * @LastEditTime: 2020-03-29 13:14:46
+ * @LastEditTime: 2020-03-29 22:32:44
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \vue-cli-demo\src\main.js
  */
-console.log('Hello vue-cli-demo')
+import Vue from 'vue'
+import App from './App.vue'
+
+const vm = new Vue({
+  render: h => h(App)
+}).$mount('#app')
